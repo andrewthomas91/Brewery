8dd0940fc92086fd7555438f6a193ed4235132f4
Andrew Thomas <andrew.thomas@workday.com>
2017-06-02 15:05:00 -0700

    Adding list view

--- app/src/main/java/andrewthomas/brewery/MainActivity.java
+++ app/src/main/java/andrewthomas/brewery/MainActivity.java
@@ -1,7 +1,11 @@
 package andrewthomas.brewery;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listView, View view, int position, long id) {
+                final String recipeTitle = listView.getItemAtPosition(position).toString();
+                Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+                intent.putExtra("RecipeTitle", recipeTitle);
+                startActivity(intent);
+            }
+        };
+        listView.setOnItemClickListener(itemClickListener);
+
     }
 }
